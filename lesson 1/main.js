1470177b3d7ac8437dd6c876dd8d728513d764e2
Vasyliv89 <Prodavecgoda@gmail.com>
2022-07-25 18:24:14 +0300

    Initial commit

---
+++ lesson 1/main.js
@@ -0,0 +1,101 @@
+let hello;
+hello =101;
+console.log(hello);
+document.write('hello');
+alert(hello);
+
+let owu;
+owu = 102;
+console.log(owu);
+alert(owu);
+document.write(' owu');
+
+let com;
+com = 103;
+console.log('com');
+alert(com);
+document.write(' com');
+
+
+let ua;
+ua=104;
+console.log('ua');
+alert(ua);
+document.write(' ua');
+
+let n1=1;
+console.log(1);
+document.write(' 1');
+alert(1);
+
+let n2=10;
+console.log(10);
+document.write(' 10');
+alert(10);
+
+
+let n3=-999;
+console.log(-999);
+document.write(' -999');
+alert(-999);
+
+
+
+let n4=123;
+console.log(123);
+document.write(' 123');
+alert(123);
+
+
+let n5=3.14;
+console.log(3.14);
+document.write(' 3.14');
+alert(3.14);
+
+
+let n6=2.7;
+console.log(2.7);
+document.write(' 2.7');
+alert(2.7);
+
+
+let n7=16;
+console.log(16);
+document.write(' 16');
+alert(16);
+
+
+let n8=true;
+console.log(true);
+document.write(' true');
+alert(true);
+
+
+
+let n9=false;
+console.log(false);
+document.write(' false');
+alert(false);
+
+let srt1='Vasyliv'
+let srt2=' Oleksandr'
+let srt3=' Volodumurovuch'
+let person=srt1+srt2+srt3
+console.log(person)
+
+
+let a = 100;
+console.log(typeof a)
+let b = '100';
+console.log(typeof'100')
+let c = true;
+console.log(typeof 'true')
+
+
+console.log(prompt(srt2))
+
+console.log(prompt(srt3))
+
+console.log(promt("33"))
+
+
